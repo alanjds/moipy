9bf6bd0f6b2f675148939b5b71a459f2d1fba786
Alan Justino da Silva <alan.justino@yahoo.com.br>
2013-07-16 22:57:11 -0300

    [FIX] setup.py had no install_requires
    Added myself as a contributor too

--- setup.py
+++ setup.py
@@ -3,12 +3,16 @@
 from distutils.core import setup
 
 setup(name='Moipy',
-      version='0.2',
+      version='0.2.1',
       description='Python integration with MoIP payment gateway via API',
-      author=['Herberth Amaral','Ale Borba','Victor'],
-      author_email=['herberthamaral@gmail.com','ale.alvesborba@gmail.com','vitalbh@gmail.com'],
-      url='http://labs.moip.com.br/',
+      author=['Herberth Amaral','Ale Borba','Victor', 'Alan Justino'],
+      author_email=['herberthamaral@gmail.com','ale.alvesborba@gmail.com','vitalbh@gmail.com', 'alan.justino@yahoo.com.br'],
+      url='https://github.com/alanjds/moipy/',
       packages=['moipy'],
+      install_requires=[
+          'lxml',
+          'pycurl',
+      ],
       classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Console',
